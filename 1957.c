22befb56a87eec46ac6c49ad97b97298ebe21625
Emon <emon22205101732@diu.edu.bd>
2023-10-02 04:15:18 +0600

    done

--- 1957.c
+++ 1957.c
@@ -1,4 +1,10 @@
 #include<stdio.h>
 int main(){
-    printf("Emon");
+   int V;
+    printf("Enter a decimal number: ");
+    scanf("%u", &V);
+
+    printf("Hexadecimal representation: %X\n", V);
+
+    return 0;
 }
